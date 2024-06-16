8962918055019420c24718defb5fea4cb8a96ff7
Dulithi Jayasooriya <129371864+Dulithi@users.noreply.github.com>
2024-06-16 22:16:56 +0530

    Use Google OAuth2.0 authentication

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import "dotenv/config";
-import express from 'express';
+import express, { request } from 'express';
 import mongoose from 'mongoose';
 import session from "express-session";
 import passport from "passport";
 import passportLocalMongoose from "passport-local-mongoose";
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import findOrCreate from "mongoose-findorcreate";
 
 
 const app = express();
@@ -15,7 +17,8 @@ app.use(express.static("public"));
 app.use(session({
     secret: process.env.LONG_SECRET_KEY,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    // cookie: { secure: process.env.NODE_ENV === 'production' }
   }));
 
 app.use(passport.initialize());
@@ -33,18 +36,56 @@ await mongoose.connect(uri);
 const userSchema = new mongoose.Schema({
     email : String,
     password: String,
+    googleId: String // for google authentication
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        done(null, user);
+    } catch (err) {
+        done(err, null);
+    }
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  (accessToken, refreshToken, profile, cb) => {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", (req, res) => {
     res.render("home");
+});
+
+app.get("/auth/google", 
+    passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets",  
+    passport.authenticate('google', { failureRedirect: '/login'}),
+    function(req, res) {
+    // Successful authentication, redirect Secrets.
+    res.redirect('/secrets');
 });
 
 app.get("/register", (req, res) => {
@@ -57,6 +98,7 @@ app.get("/login", (req, res) => {
 
 
 app.get("/secrets", (req, res) => {
+    console.log(req.isAuthenticated());
     if(req.isAuthenticated()) {
         res.render("secrets");
     } else {
